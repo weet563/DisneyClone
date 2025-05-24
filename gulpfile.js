2c81efc05833e70fb49b45ecd2253a9c6d9de5a1
Wesley <Wesley@teste.com>
2025-05-24 14:57:00 -0300

    Criando o hero e add o font Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,25 @@
 //Add requisitos dos plugins 
 const gulp = require('gulp'); //Gulp 
 const sass = require('gulp-sass')(require('sass')); //Sass
+const imagemin = require('gulp-imagemin'); //Imagemin
 
 //Compilar o sass
 function styles() {
     return gulp.src('./src/styles/*.scss') //Local de origem - antes add 'Gulp'
-        .pipe(sass({ outputStyle: 'compressed' })) //Function para comprimir
+        .pipe(sass({ outputStyle: 'compressed' })) //Function para compilar
         .pipe(gulp.dest('./dist/css')); //Local de destino - antes add 'Gulp'
 }
 
-exports.default = styles;
+//Comprimir images
+function images() {
+    return gulp.src('./src/img/**/*') //Local de origem - antes add 'Gulp'
+        .pipe(imagemin()) //Function para comprimir
+        .pipe(gulp.dest('./dist/img')); //Local de destino - antes add 'Gulp'
+}
+
+exports.default = gulp.parallel(styles, images);
+
+//Assistir o CSS ao-vivo
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
